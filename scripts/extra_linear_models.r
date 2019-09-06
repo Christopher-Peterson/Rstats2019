6529ced98d191f51d28dadccc8496788ca8d2924
Christopher-Peterson <christopher.r.peterson@gmail.com>
2019-09-06 13:56:57 -0500

    No idea what this is, but let's throw it up there anyway.

--- scripts/extra_linear_models.r
+++ scripts/extra_linear_models.r
@@ -44,12 +44,6 @@ augment(sim1_mod) %>%
   geom_line(aes(x = x, y = .fitted),
             color = "red")
 
-augment(sim1_mod) %>% 
-  ggplot() +
-  # Note we're declaring aes() inside the geom_ functions, since it's different between the layers
-  geom_point(aes(x, y = y)) +
-  geom_line(aes(x = x, y = .fitted),
-            color = "red")
 # Check residuals
 augment(sim1_mod) %>% 
   ggplot(aes(x=x, y = .resid)) +
@@ -60,28 +54,39 @@ augment(sim1_mod) %>%
 ###########################
 # Lizard Exercise:
 lizards = read_csv("data/anoles.csv")
+
+lm(Diameter ~ Limb, data = lizards) %>% 
+  augment %>% ggplot(aes(x = Limb)) +
+  geom_ribbon(aes(ymin = .fitted - .se.fit * 1.96, 
+                  # This is creates the standard error around the prediction
+                  ymax = .fitted + .se.fit * 1.96),
+              fill = grey(.8))+
+  geom_point(aes(y = Diameter)) +
+  geom_line(aes(y = .fitted),
+            color = "red")
+
 # One would expect a relationship between limb length (Limb) perch characteristics (Diameter & Height)
 # Are these relationship present in the data?  Use linear models and visualize the results.
 # Are there strong relationships between morphological measurements (SVL, Tail, Mass, Limb)?  
 
 #### ANOVA/t-test (Categorical predictors) #####
-install.packages("cld")
 # lm works with categorical predictors too
 sim2
 # Visualize it
-ggplot(sim2) + aes(x, y) + geom_violin(color = gray(.8)) + geom_jitter(width = .2)
+ggplot(sim2) + aes(x, y) + 
+  geom_violin(color = gray(.8)) + 
+  geom_jitter(width = .2, height = 0)
 
 sim2_mod <- # Now let's run an ANOVA; note the code is exactly the same as sim1
   lm(formula = y ~ x, data = sim2) 
 sim2_mod # Note the coefficients: xb, xc, xd
 # These define how the mean value of categories b:d differ from the mean of category a (which is the intercept)
 anova(sim2_mod) # gives you the ANOVA table
+
 tidy(anova(sim2_mod)) # converts it to a tidy data frame
 tidy(sim2_mod) # Gives you the effect-level estimates
 
 augment(sim2_mod) 
-# install.packages("multcomp")
-library(multcomp)
 
 # Plot means with confidence intervals
 augment(sim2_mod) %>% 
@@ -93,21 +98,20 @@ augment(sim2_mod) %>%
   geom_errorbar(aes(ymin = y_lower, ymax = y_upper), 
                 width = .3, color = "red") +
   geom_point(aes(y = .fitted), size = 2, color = "red") +
-  geom_jitter(aes(y = y), width = .2)
-
-# Exercise: 
-# 95% confidence intervals are the group mean +/- 1.96 * the standard error.
-  # Modify the above code to show them.  
-  # Which groups have non-overlapping confidence intervals?
+  geom_jitter(aes(y = y), width = .2, height = 0)
 
 # You can run post-hoc tests to determine which groups are 'significantly' different.
+
+TukeyHSD(aov(sim2_mod))
+
   # We aren't going to go into the details of that here, but let's say we had the following results, suggesting groups a and d are grouped together, with b and c being unique
 signif_groups = 
-  tribble(~"x", ~"group",
+  tribble(~"x", ~"label",
             "a", "A",
             "b", "B",
             "c", "C",
             "d", "A")
+
 sim2_anova_results = augment(sim2_mod) %>% 
   mutate(y_lower  = .fitted - .se.fit, 
          y_upper  = .fitted +  .se.fit) %>% 
@@ -117,24 +121,25 @@ sim2_anova_results %>%
   mutate(y_lower  = .fitted - 1.96*.se.fit, 
          y_upper  = .fitted +  1.96*.se.fit) %>% 
   ggplot(aes(x = x)) +  
-  # We're using the same x variables for everything, so we define it in the main ggplot function. We use different y variables, so we define the aes() in each geom
   geom_errorbar(aes(ymin = y_lower, ymax = y_upper), 
                 width = .3, color = "red") +
   geom_point(aes(y = .fitted), size = 2, color = "red") +
-  geom_jitter(aes(y = y), width = .2) +
-  geom_text(aes(label = group), y = 12, size = 12) + ylim(0, 12)
+  geom_jitter(aes(y = y), width = .2, height = 0) +
+  geom_text(aes(label = group), 
+            y = 12, size = 12) + ylim(0, 12)
 # Since there's no multi-membership groups, you map group to color or another aesthetic instead.  
 
 #####################
 # Lizard Exercise:
  
 # Do different colored anoles differ in perch use (Height and Diameter?)) Visualize group means and confidence intervals. You may wish to use geom_violin() to summarize the raw data, since there's quite a lot of it.
+
 # Are there similar differences among sites?
 
 ##### Multiple predictors #####
 
 sim3
-sim3 %>% ggplot(aes(x1, y, color = x2)) + 
+sim3 %>% ggplot(aes(x = x1, y = y, color = x2)) + 
   geom_point()
 sim3 %>% ggplot(aes(x1, y)) + facet_wrap(~x2)+ 
   geom_point()
@@ -161,7 +166,8 @@ model_comparison_data =
   ) %>% 
   mutate(ci_lower = .fitted - 1.96 * .se.fit, ci_upper = .fitted + 1.96 * .se.fit)
 
-model_comparison_data %>% ggplot(aes(x = x1, color = Model)) + facet_wrap(~x2) +
+model_comparison_data %>% 
+  ggplot(aes(x = x1, color = Model)) + facet_wrap(~x2) +
   geom_point(aes(y = y), color = "black") + # raw data
     # we're defining a fixed color here because the raw data isn't part of a model
   geom_line(aes(y = .fitted)) +
