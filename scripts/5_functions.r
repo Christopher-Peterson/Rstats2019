90856dd875d710edd0ee2cc073828275bceedf3d
Christopher-Peterson <christopher.r.peterson@gmail.com>
2019-10-20 19:02:46 -0500

    Updated for 2019:
    Added a section on scope
    Added a section on programming with tidyeval (Only in the simplest form; no !!'s, quo(), etc; just the {{ }} inside a function). Just going with this should bypass a lot of the conceptual load that NSE usually entails.

--- scripts/5_functions.r
+++ scripts/5_functions.r
@@ -1,9 +1,11 @@
 # Functions! 
 library(tidyverse)
-library(cowplot) # Not sure if we need this, but why not?
+library(cowplot) 
+theme_set(theme_cowplot())
 lizards = read_csv("data/anoles.csv")
 # Most things you use in R are functions.  Example:
 
+### Function Intro ####
 mean(1:5) # The mean function takes the arguments c(1,2,3,4,5) 
         # and returns a single value, the mean
 mean(lizards$SVL)
@@ -30,8 +32,6 @@ normalize = function(x, ...) { # We have one argument (x) and dots (...)
   # I could have simplified this as (x - mean(x, ...))/sd(x, ...)
 }
 normalize(1:5)
-
-
 
 # What do dots do? They add flexibility.  For example: 
 missing_vector = c(1:5, NA) # This has a missing value in it
@@ -93,60 +93,98 @@ normalize_general(missing_vector, method = "center")
 
 #### Advice on function writing #####
 
-# You can write your own functions.
 # Functions let you avoid errors from copy/paste duplications
-# If you need to change something, you only need to modify the code in one location, which reduces errors
-# Giving a function a good name makes the code easier to read
+  # If you need to change something, you only need to modify the code in one location, 
+  # which reduces errors
+  # Giving a function a good name makes the code easier to read
 
-# For example, let's say I wanted to estimate the diversity of lizard colors at each site 
-# using Shannon diversity: exp( - sum(prop_i * log(prop_i))), 
-# where prop is the proportion of individuals of a given type
+# For example, let's say I wanted to estimate the diversity of 
+  # lizard colors at each site 
+  # using Shannon diversity: exp( - sum(prop_i * log(prop_i))), 
+  # where prop_i is the proportion of individuals of a given type
 
 # We could arguably include this code as part of a summarize command:
 
-lizards %>% group_by(Site, Color) %>% summarize(n_i = n()) %>% 
-  group_by(Site) %>% mutate(prop_i = n_i / sum(n_i)) %>% 
+lizards %>% group_by(Site, Color_morph) %>% 
+  summarize(n_i = n()) %>% 
+  mutate(prop_i = n_i / sum(n_i)) %>% 
   summarize(diversity = exp(-sum(prop_i * log(prop_i))))
 # It's not particularly clear what's happening here, though
 # And good luck sharing this w/ a collaborator who isn't familiar with the math
 
 # Alternatively, you could define a function
-shannon_diversity <- function(types) { # types is an argument of the function
+shannon_diversity <- function(x) { # x is an argument of the function
   # This is the body of a function;
   # you can define local variables here that will disappear after the function is run.
-  counts <- table(types)  # table counts the number of occurencesof each t
+  counts <- table(x)  # table counts the number of occurences of each type of x
   proportions <- counts / sum(counts)
   out <- exp(-sum(proportions * log(proportions))) 
   out # The final value that's evalulated or printed in a function is returned
 }
 lizards %>% group_by(Site) %>% # This bit of code is much more readable 
-  summarize(diversity = shannon_diversity(Color))
+  summarize(diversity = shannon_diversity(Color_morph))
 # This also makes it easy to re-use and expand
 lizards %>% group_by(Site) %>% # This bit of code is much more readable 
-  summarize(color_diversity = shannon_diversity(Color),
+  summarize(color_diversity = shannon_diversity(Color_morph),
             perch_diversity = shannon_diversity(Perch_type))
 
 
 
-# Make your function names descriptive, but not irritatingly long
+# Make your function names descriptive,
+  # RStudio can tab-complete function names, so don't make it too short to avoid typing
   # Generally, function names should be verbs and arguments should be nouns
-# It's a good idea to have your function's main data input (whether a simple vector or a data frame)
+# It's a good idea to have your function's main data input 
+  # (whether a simple vector or a data frame)
   # be the first argument, since this will let you pipe ( %>% ) data into it.
 # The less important / more optional an argument is, the later you should put it.
   # consider adding default values for the arguments that aren't essential
-  # 
 
 # Other notes:
   # You can see the code of a function if you type it without parentheses
 normalize_general  # this works for built-in functions too, though they may be a bit cryptic
 args(normalize_general) # list the arguments of a function
+formals(normalize_general) # returns the arguments in a list
 
 #### Local variable names and scoping #####
 
+# Environments: a conceptual space where values are associated with names
+
+# When you define a variable:
 important_variable = -1
+# You associate it with an environment
+# By default we're working in the global environment
+# when you try to use that variable's name
+important_variable
+# R looks for it in the current environment
+
+# Functions have their own internal environment
+scope_function1 = function(x) {
+  local_var = 1
+  x * important_variable + local_var
+}
+scope_function1(1)
+# If your code calls for a variable that isn't in the current environment, 
+  # R looks for the parent environment (where it was created)
+  # In this case, that's the global environment
+
+scope_function2 = function(x) {
+  local_var = 1
+  important_var = 10
+  x * important_var + local_var
+}
+# But the global value is hidden if there's a local variable that matches it first
+scope_function2(1)
+
+scope_function3 = function(x) {
+  important_var = 10
+  x * scope_function1(1)
+}
+scope_function3(1)
+# In this case, scope_function1 doesn't have a local variable named important_var,
+  # so it searches its parent environment (the global environment, where it was created),
+  # not the environment in which it was called
 
 important_function = function(x) {
-  
   important_varaible = 10
   x * important_variable
 }
@@ -176,19 +214,57 @@ lizards %>%
   facet_wrap(~Site, scale = "free")+
   geom_smooth(method = "lm") + geom_point()
 
-# How could you make this into a function that could plot arbitrary sites?
-# Modify this function to allow for optional arguments to 
+###########
+# Exercise:
+###########
+# Modify this block of code into a function that could plot arbitrary sites?
+# Allow the function to have optional arguments for 
   # geom_point and geom_smooth
 
-filter(lizards, Site=='A')
+#### Functions with tidyverse variables ####
+# a.k.a., tidy evaluation
 
-plot_site <- function(.data, site='A', option_1 = 1){
-  filter(lizards, Site == site) %>%
-  ggplot(aes(x = Limb, y = Height)) +
-  facet_wrap(~Site, scale = "free")+
-  geom_smooth(method = "lm") + geom_point()
+# How would you generalize the above code to show a plot of any two variables?
+
+variable_plot = function(site, x, y, ...) {
+  lizards %>% 
+    filter(Site == site) %>%
+    ggplot(aes(x = x, y = y)) +
+    facet_wrap(~Site, scale = "free")+
+    geom_smooth(method = "lm", ...) + geom_point(...)
 }
+variable_plot(site = "A", x = SVL, y = Tail)
+# This causes an error.  Why?
 
+# The variables SVL and Tail don't exist on their own; they only make sense relative
+  # to the lizards data, and the function doesn't know that.
+  # It's trying to find free variables with those names, and when it can't it 
+  # throws an error.
+  # We need a way to tell the function to trust that these names are real until 
+  # we can pass them to ggplot
 
+# For many tidyverse functions, you can do that like this:
 
+variable_plot_good = function(site, x, y, ...) {
+  lizards %>% 
+    filter(Site == site) %>%
+    ggplot(aes(x = {{x}}, y = {{y}} )) + ## Note the double braces
+    facet_wrap(~Site, scale = "free")+
+    geom_smooth(method = "lm", ...) + geom_point(...)
+}
+variable_plot_good(site = "A", x = SVL, y = Tail)
+# These double braces tell the aes() function to use the names of x and y, 
+  # not the values they (don't) point to
 
+# Create a function to filter data that's above the mean of one variable
+above_mean = function(data, variable) {
+  data %>% filter({{variable}} > mean({{variable}}, na.rm = TRUE))
+}
+lizards %>% above_mean(SVL)
+
+# To see if a function plays nicely with {{ }}, look at the help page.
+?aes
+  # The page should mention that the function supports 
+  # "quoting," "quasiquotation," "unquoting," or something similar. 
+
+##### We could use an exercise?
